7e636ca20181d390d380b1e3ac66ad0b00373ba4
Jing Lin <Jinglin82@gmail.com>
2015-09-04 07:30:27 -0400

    World prototype

--- electronic.js
+++ electronic.js
@@ -124,11 +124,50 @@ var test = {
 	}
 };
 console.log(test.addPropTo([15]));
-console.log(test.addPropTo([10]));
 
-
-
-
+var br = {
+	prop: 10,
+	addPropTo: function(array) {
+		return array.map(function(elt) {
+			return this.prop + elt;
+		}, this);
+	}
+};
+console.log(br.addPropTo([10]));
+Grid.prototype.forEach = function(f, context) {
+	for (var y = 0; y < this.height; y++) {
+		for (var x = 0; x < this.width; x++) {
+			var value = this.space[x + y * this.width];
+			if (value != null) 
+				f.call(context, value, new Vector(x, y));
+		}
+	}
+};
+World.prototype.turn = function() {
+	var acted = [];
+	this.grid.forEach(function(critter, vector) {
+		if (critter.act && acted.indexOf(critter) != -1) {
+			acted.push(critter);
+			this.letAct(critter, vector);
+		}
+	}, this);
+};
+World.prototype.letAct = function(critter, vector) {
+	var action = critter.act(new View(this, vector));
+	if (action && action.type == "move") {
+		var dest = this.checkDestination(action, vector);
+		if (dest && this.grid.get(dest) == null) {
+			this.grid.set(vector, null);
+			this.grid.set(dest, critter);
+		}
+	}
+};
+World.prototype.checkDestination = function(action, vector) {
+	if (directions.hasOwnProperty(action.direction)) {
+		var dest = vector.plus(directions[action.direction]);
+		if (this.grid.isInside(dest)) return dest;
+	}
+};
 
 
 
