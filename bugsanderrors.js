6000cf76a55a92aaaaa0a7520e65434b3748cd9d
Jing Lin <Jinglin82@gmail.com>
2015-09-05 18:26:19 -0400

    Assertions

--- bugsanderrors.js
+++ bugsanderrors.js
@@ -47,10 +47,124 @@ function numberToString(n, base) {
 		result = String(n % base) + result;
 		n /= base;
 		n = parseInt(n);
+		// UNCOMMENT THE ABOVE
 	} while (n > 0)
 	return sign + result;
 }
 console.log(numberToString(13,2));
+
+// function promptNumber(question) {
+// 	var result = Number(prompt(question, ''));
+// 	if (!isNaN(result)) return null;
+// 	else return result;
+// }
+
+// console.log(promptNumber('How many trees do you see?'));
+
+function promptDirection(question) {
+	var result = prompt(question, '');
+	if (result.toLowerCase() == 'Left') return 'L';
+	if (result.toLowerCase() == 'Right') return 'R';
+	throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+	if (promptDirection('Which way?') == 'L')
+		return 'a house';
+	else 
+		return 'two angry bears';
+}
+
+try {
+	console.log('You see', look());
+} catch (error) {
+	console.log('Something went wrong');
+}
+
+var context = null;
+function withContext(newContext, body) {
+	var prevContext = context;
+	context = newContext;
+	var result = body();
+	context = prevContext;
+	return result;
+}
+
+//What happens if body() raises an error? In that scenario, the context would never be reset to its former value.
+function withContextTwo(newContext, body) {
+	var prevContext = context;
+	context = newContext;
+	try {
+		return body();
+	} finally {
+		context = prevContext;
+	}
+}
+
+try {
+	withContextTwo(5, function() {
+		if (context < 10) 
+			throw new Error('Not enough context');
+	});
+} catch (e) {
+	console.log('Ignoring ' + e);
+}
+console.log(context);
+
+//Catching a specific type of exception
+
+// for(;;) {
+// 	try {
+// 		withContextTwo(5, function() {
+// 			if (context < 10) 
+// 				throw new Error('Not enough context');
+
+// 		});
+// 	} catch (e) {
+// 		console.log(context);
+// 	}
+// }
+
+function InputError(message) {
+	this.message = message;
+	this.stack = (new Error()).stack;
+}
+InputError.prototype = Object.create(Error.prototype);
+InputError.prototype.name = 'InputError';
+
+// for(;;) {
+// 	try {
+// 		var dir = promptDirection('Where?');
+// 		console.log('You chose ', dir);
+// 		break;
+// 	} catch (e) {
+// 		if (e instanceof InputError) 
+// 			console.log('Not a valid direction. Try again.');
+// 		else
+// 			throw e;
+// 	}
+// }
+
+function AssertionFailed(message) {
+	this.message = message;
+}
+AssertionFailed.prototype = Object.create(Error.prototype);
+
+function assert(test, message) {
+	if (!test) throw new AssertionFailed(message);
+}
+
+function lastElement(array) {
+	assert(array.length > 0, 'Empty array in lastElement');
+	return array[array.length - 1];
+}
+
+
+
+
+
+
+
 
 
 
