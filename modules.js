a09eafdb7fc51afcdef51e421fb3c6ae67a27de5
Jing Lin <Jinglin82@gmail.com>
2015-09-08 18:04:20 -0400

    updated require()

--- modules.js
+++ modules.js
@@ -87,19 +87,34 @@ backgroundReadFile.files = {
 
 var exports = {};
 
+// function require(name) {
+// 	var code = new Function('exports', readFile(name));
+// 	var exports = {};
+// 	code(exports);
+// 	return exports;
+// }
+// var weekDay = require('weekDay');
+// var today = require('today');
+
+// console.log(weekDay.name(today.dayNumber()));
+
 function require(name) {
-	var code = new Function('exports', readFile(name));
-	var exports = {};
-	code(exports);
-	return exports;
+	if (name in require.cache) 
+		return require.cache[name];
+
+	var code = new Function('exports', 'module', readFile(name));
+	var exports = {}, module = {exports: exports};
+	code(exports, module);
+
+	require.cache[name] = module.exports;
+	return module.exports;
 }
+require.cache = Object.create(null);
 
-console.log(require('weekDay').name(1));
+var weekDay = require('weekDay');
+var today = require('today');
 
-
-
-
-
+console.log(weekDay.name(today.dayNumber()));
 
 
 
