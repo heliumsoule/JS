11f24efac48aa6cc4196e1ae288636af83aed043
Jing Lin <Jinglin82@gmail.com>
2015-09-09 23:35:05 -0400

    Two more test cases

--- egg.js
+++ egg.js
@@ -171,7 +171,14 @@ specialForms['fun'] = function(args, env) {
 	};
 };
 
+run("do(define(plusOne, fun(a, +(a, 1))),",
+	"	print(plusOne(10)))");
 
+run("do(define(pow, fun(base, exp,",
+	"		if(==(exp, 0),", 
+	"			1,",
+	"			*(base, pow(base, -(exp, 1)))))),",
+	"	print(pow(2, 10)))");
 
 
 
