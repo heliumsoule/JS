e08e66c10c54192deff8a2e975b8f40ad7daf024
Jing Lin <Jinglin82@gmail.com>
2015-09-13 23:32:44 -0400

    Checking input from HTML

--- code/hello.js
+++ code/hello.js
@@ -1,1 +1,16 @@
-alert("Hello");
+// alert("Hello");
+
+function talksAbout(node, string) {
+	if (node.nodeType == document.ELEMENT_NODE) {
+		for (var i = 0; i < node.childNodes.length; i++) {
+			if (talksAbout(node.childNodes[i], string))
+				return true;
+		}
+		return false;
+	} else if (node.nodeType == document.TEXT_NODE) {
+		return node.nodeValue.indexOf(string) > -1;
+	}
+}
+
+console.log(talksAbout(document.body, "book"));
+
