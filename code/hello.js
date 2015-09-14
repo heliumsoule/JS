a80a87942d9a72bf5e78b92b5e3396e3f379d1e8
Jing Lin <Jinglin82@gmail.com>
2015-09-14 00:16:47 -0400

    Appending Elements to BlockQuote

--- code/hello.js
+++ code/hello.js
@@ -27,7 +27,7 @@ document.body.insertBefore(paragraphs[2], paragraphs[0]);
 
 function replaceImages() {
 	var images = document.getElementsByTagName('img');
-	for (var i = 0; i < images.length; i++) {
+	for (var i = images.length - 1; i >= 0; i--) {
 		var image = images[i];
 		if (image.alt) {
 			var text = document.createTextNode(image.alt);
@@ -35,6 +35,43 @@ function replaceImages() {
 		}
 	}
 }
+
+//The funciton replaceImages() needs to start from the back because the list that is being updated is live
+//so when one element is pulled out, the list size actually changes. If you start updating from the back of the collection
+//you can avoid that problem.
+
+var arrayish = {0: 'one', 1: 'two', length: 2};
+var real = Array.prototype.slice.call(arrayish, 0);
+
+function elt(type) {
+	var node = document.createElement(type);
+	for (var i = 1; i < arguments.length; i++) {
+		var child = arguments[i];
+		if (typeof child == "string")
+			child = document.createTextNode(child);
+		node.appendChild(child);
+	}
+
+	return node;
+}
+
+console.log(document.getElementById('quote'));
+document.getElementById('quote').appendChild(
+	elt('footer', '---',
+		elt('strong', 'Karl Popper'),
+		', preface to the second edition of ',
+		elt('em', 'The Open Society and Its Enemies'),
+		', 1950'));
+
+
+
+
+
+
+
+
+
+
 
 
 
