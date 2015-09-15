093c465ee59bf0d5d6f2fd67750f6af2c4c41ed2
Jing Lin <Jinglin82@gmail.com>
2015-09-15 18:46:42 -0400

    Example JS highlighting

--- code/script.js
+++ code/script.js
@@ -87,9 +87,21 @@ function highlightCode(node, keywords) {
 	node.appendChild(document.createTextNode(after));
 }
 
+var languages = {
+	javascript: /\b(function|return|var)\b/g 
+};
 
+function highlightAllCode() {
+	var pres = document.body.getElementsByTagName('pre');
+	for (var i = 0; i < pres.length; i++) {
+		var pre = pres[i];
+		var lang = pre.getAttribute('data-language');
+		if (languages.hasOwnProperty(lang))
+			highlightCode(pre, languages[lang]);
+	}
+}
 
-
+highlightAllCode();
 
 
 
