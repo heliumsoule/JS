8025740a589761f67511ad4d2db201c3267253d3
Jing Lin <Jinglin82@gmail.com>
2015-09-16 11:49:27 -0400

    Violet

--- events.js
+++ events.js
@@ -2,22 +2,21 @@
 // 	console.log('You clicked');
 // });
 
-// var button = document.querySelector('button');
-// button.addEventListener('click', function() {
-// 	console.log('button clicked');
-// });
+var a = document.getElementsByTagName('button')[0];
+a.addEventListener('click', function() {
+	a.log('button clicked');
+});
 
 //One time button
-// var button = document.querySelector('button');
-// button.addEventListener('click', once);
-// function once() {
-// 	console.log('Done.');
-// 	button.removeEventListener('click', once);
-// }
+var b = document.getElementsByTagName('button')[1];
+b.addEventListener('click', once);
+function once() {
+	console.log('Done.');
+	b.removeEventListener('click', once);
+}
 
-var button = document.querySelector('button');
-button.addEventListener('mousedown', function(event) {
-	console.log(event);
+var c = document.getElementsByTagName('button')[2];
+c.addEventListener('mousedown', function(event) {
 	if (event.which == 1)
 		console.log('Left button');
 	else if (event.which == 2) 
@@ -25,6 +24,45 @@ button.addEventListener('mousedown', function(event) {
 	else if (event.which == 3)
 		console.log('Right button');
 });
+
+//Handler Propagation
+var para = document.getElementsByTagName('p')[2];
+var button = document.getElementsByTagName('button')[3];
+para.addEventListener('mousedown', function() {
+	console.log('Handler for paragraph.');
+});
+button.addEventListener('mousedown', function(event) {
+	console.log('Handler for button.');
+	if (event.which == 3)
+		event.stopPropagation();
+});
+
+document.body.addEventListener('click', function(event) {
+	if (event.target.nodeName == 'BUTTON') 
+		console.log('Clicked', event.target.textContent);
+});
+
+//Prevent default actions
+
+var link = document.querySelector('a');
+link.addEventListener('click', function(event) {
+	console.log('Nope');
+	event.preventDefault();
+});
+
+addEventListener('keydown', function(event) {
+	if (event.keyCode == 86) {
+		document.body.style.background = 'violet';
+		console.log('test');
+	}
+});
+addEventListener('keyup', function(event) {
+	if (event.keyCode == 86)
+		document.body.style.background = '';
+});
+
+
+
 
 
 
